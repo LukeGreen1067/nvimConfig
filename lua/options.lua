6a22eb58efd34bb715ba64c199c1a553a2d17a0c
Administrator <gitlab_admin_3dc30b@example.com>
2025-03-12 16:12:37 +0000

    inital commit

---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
